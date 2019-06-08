4903c33d26e86bc7df293391abc37eb6f6413962
annie <annie@canva.com>
2019-06-08 12:30:34 +1000

    type annotations, added string

--- lister.ts
+++ lister.ts
@@ -1,4 +1,6 @@
-function lister(task) {
+// Added `: string`
+// Type annotations in TypeScript are lightweight ways to record the intended contract of the function or variable
+function lister(task: string) {
     return "Hello, " + task;
 }
 
